93cb16f60344da7eaf329c37bf0bd3458fc4b6d0
sergicurtu <sergi.curtu@numancia.cat>
2017-10-29 21:21:43 +0100

    audio

--- js/index.js
+++ js/index.js
@@ -3,43 +3,45 @@ $(document).on('deviceready', function() {
 	alert("recording now");
 	recordAudio();
 	
+		function recordAudio() {
+			var src = "myrecording.amr";
+			var mediaRec = new Media(src, onSuccess, onError);
 
+			// Record audio
+			mediaRec.startRecord();
+
+			// Stop recording after 10 sec
+			var recTime = 0;
+			var recInterval = setInterval(function() {
+				recTime = recTime + 1;
+				setAudioPosition(recTime + " sec");
+				if (recTime >= 10) {
+					clearInterval(recInterval);
+					mediaRec.stopRecord();
+				}
+			}, 1000);
+		}
+
+		// onSuccess Callback
+		//
+		function onSuccess() {
+			console.log("recordAudio():Audio Success");
+			alert("recordAudio():Audio Success");
+		}
+
+		// onError Callback
+		//
+		function onError(error) {
+			alert('code: '    + error.code    + '\n' +
+				  'message: ' + error.message + '\n');
+		}
+
+		// Set audio position
+		//
+		function setAudioPosition(position) {
+			//document.getElementById('audio_position').innerHTML = position;
+		}
+	
+	
 });
 
-function recordAudio() {
-        var src = "myrecording.amr";
-        var mediaRec = new Media(src, onSuccess, onError);
-
-        // Record audio
-        mediaRec.startRecord();
-
-        // Stop recording after 10 sec
-        var recTime = 0;
-        var recInterval = setInterval(function() {
-            recTime = recTime + 1;
-            setAudioPosition(recTime + " sec");
-            if (recTime >= 10) {
-                clearInterval(recInterval);
-                mediaRec.stopRecord();
-            }
-        }, 1000);
-}
-
-// onSuccess Callback
-//
-function onSuccess() {
-	console.log("recordAudio():Audio Success");
-}
-
-// onError Callback
-//
-function onError(error) {
-	alert('code: '    + error.code    + '\n' +
-		  'message: ' + error.message + '\n');
-}
-
-// Set audio position
-//
-function setAudioPosition(position) {
-	document.getElementById('audio_position').innerHTML = position;
-}
