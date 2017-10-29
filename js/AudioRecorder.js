5565cc5f9a19dbbc2684c4282cccf42d6201ae06
sergicurtu <sergi.curtu@numancia.cat>
2017-10-29 21:40:45 +0100

    media recorder

--- js/AudioRecorder.js
+++
@@ -1,9 +0,0 @@
-var exec = require('cordova/exec');
-
-exports.startRecord = function(time, success, error) {
-    exec(success, error, "AudioRecorder", "startRecord", [time]);
-};
-
-exports.stopRecord = function(success, error) {
-    exec(success, error, "AudioRecorder", "stopRecord", []);
-};
