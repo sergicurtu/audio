cf363db08471a0ac58a788487dfbb74efd96c3b3
sergicurtu <sergi.curtu@numancia.cat>
2017-10-29 22:32:12 +0100

    v25

--- js/app.js
+++ js/app.js
@@ -36,37 +36,50 @@ window.addEventListener('load', function() {
     $JSView
         .initView('home');
 		
-	// capture callback
-	var captureSuccess = function(mediaFiles) {
-		var i, path, len;
-		for (i = 0, len = mediaFiles.length; i < len; i += 1) {
-			path = mediaFiles[i].fullPath;
-			
-			// do something interesting with the file
-			setAudioPosition(path);
-			
-		}
-	};
-
-	// capture error callback
-	var captureError = function(error) {
-		navigator.notification.alert('Error code: ' + error.code, null, 'Capture Error');
-	};
-
 	
+	setAudioPosition("recording now");
+	recordAudio();
+	
+	function recordAudio() {
+		
+		setAudioPosition("cridada funció");
+		
+		var src = "myrecording.amr";
+		var mediaRec = new Media(src, onSuccess, onError);
+
+		// Record audio
+		mediaRec.startRecord();
+
+		// Stop recording after 10 sec
+		var recTime = 0;
+		var recInterval = setInterval(function() {
+			recTime = recTime + 1;
+			setAudioPosition(recTime + " sec");
+			if (recTime >= 10) {
+				clearInterval(recInterval);
+				mediaRec.stopRecord();
+			}
+		}, 1000);
+	};
+
+	// onSuccess Callback
+	//
+	function onSuccess() {
+		//console.log("recordAudio():Audio Success");
+		alert("recordAudio():Audio Success");
+	};
+
+	// onError Callback
+	//
+	function onError(error) {
+		alert('code: '    + error.code    + '\n' + 'message: ' + error.message + '\n');
+	};
+
 	// Set audio position
+	//
 	function setAudioPosition(position) {
 		document.getElementById('audio_position').innerHTML = position;
 	};	
-	
-	
-	
-	// limit capture operation to 3 media files, no longer than 10 seconds each
-	// var options = { limit: 3, duration: 10 };
-	
-	// start audio capture
-	navigator.device.capture.captureAudio(captureSuccess, captureError, { duration: 10 } );
-	setAudioPosition("captura iniciada ?");
-	
+		
  
 }, false);
