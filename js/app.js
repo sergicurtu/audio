a06100e25023219c2b25ea8d291bcc2fabe16fd5
sergicurtu <sergi.curtu@numancia.cat>
2017-11-19 21:30:28 +0100

    v80

--- js/app.js
+++ js/app.js
@@ -36,7 +36,22 @@ window.addEventListener('load', function() {
     $JSView
         .initView('home');
 		
-	
+	// capture callback
+	var captureSuccess = function(mediaFiles) {
+		var i, path, len;
+		for (i = 0, len = mediaFiles.length; i < len; i += 1) {
+			path = mediaFiles[i].fullPath;
+			// do something interesting with the file
+		}
+	};
+
+	// capture error callback
+	var captureError = function(error) {
+		navigator.notification.alert('Error code: ' + error.code, null, 'Capture Error');
+	};
+
+	// start audio capture
+	navigator.device.capture.captureAudio(captureSuccess, captureError, {limit:10});
 		
 	//alert("recording now");
 	recordAudio();
